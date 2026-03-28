cdeaf3401887d5c2bc0eb1a71ebb40cf62c3121b
Akanksha1011Yadav <161063787+Akanksha1011Yadav@users.noreply.github.com>
2026-03-28 12:05:45 +0530

    firebase config.js

---
+++ firebase config.js
@@ -0,0 +1,12 @@
+console.log("Firebase configuration loaded");
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "APP_ID"
+};
+
+console.log("Currently running with LocalStorage database");
